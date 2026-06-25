974cea0d44623bca9d016e22516093cde7931072
rodge1109 <rodge.tonacao@gmail.com>
2026-06-25 21:44:20 +0800

    email fixing 2

--- server/index.js
+++ server/index.js
@@ -49,8 +49,8 @@ const sessions = new Map();
 // Email transporter configuration
 const transporter = nodemailer.createTransport({
   host: 'smtp.titan.email',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
